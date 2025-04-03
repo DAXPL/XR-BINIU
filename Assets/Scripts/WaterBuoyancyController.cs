0a6cf8ee2eb612db949969d1b6b92a47d5f6e1c5
Cosinus215 <cosinus215@gmail.com>
2025-04-03 03:07:26 +0300

    Add comments, tooltips and headers

--- Assets/Scripts/WaterBuoyancyController.cs
+++ Assets/Scripts/WaterBuoyancyController.cs
@@ -2,24 +2,46 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.HighDefinition;
 
+[RequireComponent(typeof(Rigidbody))]
 public class WaterBuoyancyController : MonoBehaviour {
-    [SerializeField] private Rigidbody rb;
-    [SerializeField] private float submersionDepth = 4;
-    [SerializeField] private float submergedForceMultipier = 2;
-    [SerializeField] private float waterDrag = 2;
-    [SerializeField] private float waterAngularDrag = 2;
+    [Header("Components")]
     [SerializeField] private WaterSurface water;
 
-    private List<Vector3> vertices = new();
-    private Vector3 totalDragForce;
+    [Header("Buoyancy Settings")]
+    [Tooltip("How much of the object will be submerged.")]
+    [SerializeField] private float submersionDepth = 4;
+
+    [Tooltip("How strong will the object bounce back up when submerged.")]
+    [SerializeField] private float submergedForceMultipier = 2;
+
+    [Tooltip("Slows down movement in water. Higher values make the object stop faster.")]
+    [SerializeField] private float waterDrag = 2;
+
+    [Tooltip("Slows down rotation in water. Higher values make the object rotate less.")]
+    [SerializeField] private float waterAngularDrag = 2;
+
+    private Rigidbody rb;
+    private readonly List<Vector3> vertices = new();
+    private Vector3 totalForce;
     private Vector3 totalTorque;
+    private int submergedFloaters;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody>();
+
+        if (water == null) {
+            Debug.LogError("No water surface attached.");
+        }
+    }
 
     private void Start() {
         MeshFilter meshFilter = GetComponent<MeshFilter>();
         foreach (Vector3 vertex in meshFilter.mesh.vertices) {
+            // Ignore vertices above object's local level of water
             if (vertex.y > 0)
                 continue;
 
+            // Skip loop to avoid duplicate values in the list
             if (vertices.Contains(vertex))
                 continue;
 
@@ -28,42 +50,48 @@ public class WaterBuoyancyController : MonoBehaviour {
     }
 
     private void FixedUpdate() {
-        int submergedFloaters = 0;
-        totalDragForce = Vector3.zero;
+        // Reset values
+        submergedFloaters = 0;
+        totalForce = Vector3.zero;
         totalTorque = Vector3.zero;
 
         foreach (Vector3 vertex in vertices) {
-            Vector3 floaterPos = transform.TransformPoint(vertex);
-            // Get water height at floater position
+            // Transform vertex from local to world space
+            Vector3 vertexPos = transform.TransformPoint(vertex);
+
+            // Get water height at vertex position
             WaterSearchParameters projectionParams = new() {
-                startPositionWS = floaterPos
+                startPositionWS = vertexPos
             };
 
+            // Skip loop if vertex is not over a water surface
             if (!water.ProjectPointOnWaterSurface(projectionParams, out WaterSearchResult projectionResult))
                 continue;
 
+            // Get water height at vertex
             float waterHeight = projectionResult.projectedPositionWS.y;
 
-            // If floater is underwater, apply buoyancy
-            if (floaterPos.y < waterHeight) {
+            // If vertex is underwater apply buoyancy
+            if (vertexPos.y < waterHeight) {
                 submergedFloaters++;
 
-                // Calculate buoyancy force for this floater
-                float submergedAmount = Mathf.Clamp01((waterHeight - floaterPos.y) / submersionDepth);
-                Vector3 buoyancyForce = Mathf.Abs(Physics.gravity.y) * submergedAmount * submergedForceMultipier * Vector3.up;
+                // Calculate buoyancy force for vertex
+                float submergedAmount = Mathf.Clamp01((waterHeight - vertexPos.y) / submersionDepth);
+                Vector3 buoyancyForce = 
+                    Mathf.Abs(Physics.gravity.y) * submergedAmount * submergedForceMultipier * Vector3.up;
 
-                // Add drag forces
-                totalDragForce += Time.fixedDeltaTime * waterDrag * -rb.linearVelocity;
+                // Add forces to total force vector at position of this vertex
+                totalForce += Time.fixedDeltaTime * waterDrag * -rb.linearVelocity;
                 totalTorque += Time.fixedDeltaTime * waterAngularDrag * -rb.angularVelocity;
 
-                // Apply force at floater position
-                rb.AddForceAtPosition(buoyancyForce, floaterPos, ForceMode.Acceleration);
+                // Apply buoyancy force at the vertex position
+                rb.AddForceAtPosition(buoyancyForce, vertexPos, ForceMode.Acceleration);
             }
         }
 
-        // Apply total drag once to prevent excessive dampening
+        // Apply total water resistance Y force at once if any vertex is submerged
         if (submergedFloaters > 0) {
-            rb.AddForce(totalDragForce, ForceMode.VelocityChange);
+            rb.AddForce(totalForce, ForceMode.VelocityChange);
             rb.AddTorque(totalTorque, ForceMode.VelocityChange);
         }
     }
