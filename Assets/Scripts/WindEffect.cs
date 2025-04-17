476f542d467c77d31b8ddefb10326d5b70fd486a
Cosinus215 <cosinus215@gmail.com>
2025-04-17 21:49:04 +0300

    Add comments

--- Assets/Scripts/WindEffect.cs
+++ Assets/Scripts/WindEffect.cs
@@ -1,13 +1,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class WindEffect : MonoBehaviour {
-    [SerializeField] private float xScale;
-    [SerializeField] private float speed;
+    [Header("Wind strength variables")]
+    [SerializeField] private float xScale = 1f;
+    [SerializeField] private float speed = 1f;
+
+    [Tooltip("Wind blowing direction")]
     [SerializeField] private Vector3 directionVector;
+
     private readonly List<Vector3> vertices = new();
     private Rigidbody rb;
     private Vector3 forceVector;
+    private float windScale;
 
     private void Awake() {
         rb = GetComponent<Rigidbody>();
@@ -17,7 +23,7 @@ public class WindEffect : MonoBehaviour {
         GetAllVertices();
     }
 
-    public void GetAllVertices() {
+    private void GetAllVertices() {
         vertices.Clear();
         MeshFilter meshFilter = GetComponent<MeshFilter>();
         foreach (Vector3 vertex in meshFilter.mesh.vertices) {
@@ -31,17 +37,33 @@ public class WindEffect : MonoBehaviour {
     }
 
     private void FixedUpdate() {
+        // Calculate base wind force vector
         forceVector = directionVector.normalized * speed;
+
+        // Loop through each vertex
         foreach (Vector3 vertex in vertices) {
+            // Get vertex world position
             Vector3 worldVertex = transform.TransformPoint(vertex) - directionVector / 10;
+
+            // Add offset for raycast
             directionVector.y += vertex.y;
 
-            // Check if something is in front of vertex
+            // Skip applying force if there is an obstacle in front of the vertex
             if (Physics.Raycast(worldVertex, -directionVector, 10))
                 continue;
 
-            forceVector *= Mathf.PerlinNoise(Time.fixedTime * xScale, 0.0f);
+            windScale = 1;
+
+            // Check if there is an obstacle behind the vertex
+            if (Physics.Raycast(worldVertex, directionVector, out RaycastHit hit, 10))
+                windScale = hit.distance/10;
+
+            // Add randomized wind to base force
+            forceVector *= Mathf.PerlinNoise(Time.fixedTime * xScale, 0.0f) * windScale;
+
+            // Apply force at the vertex position
             rb.AddForceAtPosition(forceVector, vertex);
         }
     }
+
 }
